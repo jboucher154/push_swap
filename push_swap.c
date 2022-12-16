55d869eabe7bac04a06af0dde8db33bd4075cf13
Jenny Boucher <jebouche@c2r3p4.hive.fi>
2022-12-16 15:01:37 +0200

    add outline for entry to sorting algo function

--- push_swap.c
+++ push_swap.c
@@ -6,39 +6,62 @@
 /*   By: jebouche <jebouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 15:31:21 by jebouche          #+#    #+#             */
-/*   Updated: 2022/12/15 15:40:39 by jebouche         ###   ########.fr       */
+/*   Updated: 2022/12/16 14:42:09 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sort_list(t_list *lst_a)
+void	make_move(t_list **moves, t_list **lst_a, t_list **lst_b)
+{
+	
+}
+
+t_list	*sort_list(t_list *lst_a)
 {
 	t_list	*lst_b;
+	t_list	*moves;
+	t_list	*new;
+	int		len;
+	int 	i;
 
 	lst_b = NULL;
-	if (lst_a->next == NULL) // or just check if sorted here...
+	moves = NULL;
+	i = 0;
+	len = ft_lstsize(lst_a);
+	if (check_if_sorted(lst_a)) //check if sorted here...
 		return ;//list is already sorted
 	else
 	{
-		return ;
-		//check if sorted already?
+		while (i <= len / 2)
+		{
+			new = ft_lstnew(pb(&lst_b, &lst_a));
+			if (new)
+				ft_lstadd_back(&moves, new);
+			else
+			{
+				return ;
+				//free and exit;
+			}
+			i++;
+		}
+		// make_move();
 		//otherwse start the sorting?
 	}
+	//free other lists before return
+	return (moves);
 }
 
 int	main(int argc, char **argv)
 {
 	t_list	*head;
-	// int		sorted;
 
 	head = NULL;
 	if (argc > 1)
 		head = read_inputs(argc, argv);
 	if (head == NULL)
 		ft_printf("Error\n");
-	// sorted = check_if_sorted(head);
-	// ft_printf("sorted: %i\n", sorted);
+	
 	print_list(head);
 	ft_lstclear(&head, &del_int_content);
 	return (0);
