ba87bfb06c5841d8f451f12dc657b2111fbd7412
Jenny Boucher <jebouche@c2r3p4.hive.fi>
2022-12-20 18:37:39 +0200

    add check reverse sorted

--- check_if_sorted.c
+++ check_if_sorted.c
@@ -6,7 +6,7 @@
 /*   By: jebouche <jebouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 15:38:58 by jebouche          #+#    #+#             */
-/*   Updated: 2022/12/15 15:39:01 by jebouche         ###   ########.fr       */
+/*   Updated: 2022/12/19 17:13:46 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,38 @@ int	check_if_sorted(t_list *head)
 	int		len;
 	t_list	*temp;
 
-	len = ft_lstsize(head);
-	ft_printf("length : %i\n", len);
+	// ft_printf("length : %i\n", len);
 	if (!head)
 		return (0);
 	temp = head;
-	while (temp->next && ((*(int *)temp->data) < (*(int *)temp->next->data)))
+	len = ft_lstsize(temp);
+	while (temp->next && ((*(int *)temp->data) <= (*(int *)temp->next->data)))
 	{
 		len--;
 		temp = temp->next;
 	}
-	ft_printf("length after check : %i\n", len);
+	// ft_printf("length after check : %i\n", len);
+	if (len == 1)
+		return (1);
+	return (0);
+}
+
+int	check_if_revsorted(t_list *head)
+{
+	int		len;
+	t_list	*temp;
+	
+	// ft_printf("length : %i\n", len);
+	if (!head)
+		return (0);
+	temp = head;
+	len = ft_lstsize(temp);
+	while (temp->next && ((*(int *)temp->data) >= (*(int *)temp->next->data)))
+	{
+		len--;
+		temp = temp->next;
+	}
+	// ft_printf("length after check : %i\n", len);
 	if (len == 1)
 		return (1);
 	return (0);
