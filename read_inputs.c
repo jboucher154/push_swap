d990bd77aed6a64897130cf78c1c042a01a86dce
Jenny Boucher <jebouche@c2r3p4.hive.fi>
2022-12-22 17:21:22 +0200

    add error checking for ob ints and dups

--- read_inputs.c
+++ read_inputs.c
@@ -6,7 +6,7 @@
 /*   By: jebouche <jebouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 11:58:36 by jebouche          #+#    #+#             */
-/*   Updated: 2022/12/21 12:01:00 by jebouche         ###   ########.fr       */
+/*   Updated: 2022/12/22 17:12:02 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,49 @@ static int	is_int(char *to_check)
 	return (0);
 }
 
+static int	check_dups(t_list	**stack)
+{
+	t_list	*temp1;
+	t_list	*temp2;
+	int		to_check;
+
+	if (!stack)
+		return (0);
+	temp1 = *stack;
+	temp2 = (*stack)->next;
+	while (temp1 && temp2)
+	{
+		to_check = *(int *)temp1->data;
+		while (temp2)
+		{
+			if (to_check == *(int *)temp2->data)
+				return (0);
+			temp2 = temp2->next;
+		}
+		temp1 = temp1->next;
+		temp2 = temp1->next;
+	}
+	return (1);
+}
+
+static int	check_num(int num, char *str)
+{
+	if (num < 0 && str[0] != '-')
+		return (0);
+	else if (num > 0 && str[0] == '-')
+		return (0);
+	return (1);
+}
+
 static int	generate_stack(char **strs, t_list **head, int arg_str)
 {
 	t_list	*new;
 	int		i;
 	int		num;
 
+	i = 0;
 	if (arg_str == 0)
 		i = 1;
-	else
-		i = 0;
 	if (!head)
 		return (0);
 	num = 0;
@@ -44,12 +77,16 @@ static int	generate_stack(char **strs, t_list **head, int arg_str)
 		if (!is_int(strs[i]))
 			return (0);
 		num = ft_atoi(strs[i]);
+		if (!check_num(num, strs[i]))
+			return (0);
 		new = ft_lstnew(make_int_content(num));
 		if (!new)
 			return (0);
 		ft_lstadd_back(head, new);
 		i++;
 	}
+	if (!check_dups(head))
+		return (0);
 	return (1);
 }
 
@@ -75,3 +112,32 @@ t_list	*read_inputs(int argc, char **argv)
 	}
 	return (head);
 }
+
+
+// static int	check_errors(t_list	**stack)
+// {
+// 	t_list	*temp1;
+// 	t_list	*temp2;
+// 	int		to_check;
+
+// 	if (!stack)
+// 		return (0);
+// 	temp1 = *stack;
+// 	temp2 = (*stack)->next;
+// 	while (temp1 && temp2)
+// 	{
+// 		to_check = *(int *)temp1->data;
+// 		while (temp2)
+// 		{
+// 			if (to_check == *(int *)temp2->data)
+// 				return (0);
+// 			else if (to_check > INT_MAX || to_check < INT_MIN || \
+// 				*(int *)temp2->data > INT_MAX || *(int *)temp2->data > INT_MIN)
+// 				return (0);
+// 			temp2 = temp2->next;
+// 		}
+// 		temp1 = temp1->next;
+// 		temp2 = temp1->next;
+// 	}
+// 	return (1);
+// }
