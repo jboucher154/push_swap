398e4f16d7dad372cd10d9fac681998863d93b29
Jenny Boucher <jebouche@c2r3p4.hive.fi>
2022-12-27 17:59:55 +0200

    remove use of list len

--- sorting_utility.c
+++ sorting_utility.c
@@ -6,7 +6,7 @@
 /*   By: jebouche <jebouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 11:51:58 by jebouche          #+#    #+#             */
-/*   Updated: 2022/12/22 17:06:49 by jebouche         ###   ########.fr       */
+/*   Updated: 2022/12/27 11:02:39 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,11 @@ void	add_move(t_list **moves, char *move)
 	ft_lstadd_back(moves, new);
 }
 
-void	check_swaps_a(t_list **moves, t_list **lst_a, t_list **lst_b, int *ab_len)
+void	check_swaps_a(t_list **moves, t_list **lst_a, t_list **lst_b)
 {
-	if (ab_len[0] > 1 && *((int *)(*lst_a)->data) > *((int *)(*lst_a)->next->data))
+	if (!lst_a || !(*lst_a))
+		return ;
+	if ((*lst_a)->next && *((int *)(*lst_a)->data) > *((int *)(*lst_a)->next->data))
 	{
 		if ((*lst_b) && (*lst_b)->next && *((int *)(*lst_b)->data) < *(int *)(*lst_b)->next->data)
 			add_move(moves, ss(lst_a, lst_b));
@@ -33,19 +35,15 @@ void	check_swaps_a(t_list **moves, t_list **lst_a, t_list **lst_b, int *ab_len)
 	}
 }
 
-void	check_swaps_b(t_list **moves, t_list **lst_a, t_list **lst_b, int *ab_len)
+void	check_swaps_b(t_list **moves, t_list **lst_a, t_list **lst_b)
 {
-	if (lst_b == NULL || *lst_b == NULL || (*lst_b)->next == NULL)
+	if (lst_b == NULL || *lst_b == NULL)
 		return ;
-	if (ab_len[1] > 1 && *((int *)(*lst_b)->data) < *((int *)(*lst_b)->next->data))
+	if ((*lst_b)->next && *((int *)(*lst_b)->data) < *((int *)(*lst_b)->next->data))
 	{
-		if (ab_len[1] == 2 && ab_len[0] > 2)
-		{
+		if ((*lst_b)->next->next != NULL && (*lst_a)->next->next != NULL)
 			add_move(moves, rrr(lst_a, lst_b));
-		}
 		else
-		{
 			add_move(moves, sb(lst_b));
-		}
 	}
 }
