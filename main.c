92679be544718aff970bb7bb33b9f2e6d85f3e5a
Jenny Boucher <jebouche@c2r3p4.hive.fi>
2023-01-04 13:56:43 +0200

    add printsanitized, error exit

--- main.c
+++ main.c
@@ -1,16 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jebouche <jebouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 15:31:21 by jebouche          #+#    #+#             */
-/*   Updated: 2023/01/03 15:04:07 by jebouche         ###   ########.fr       */
+/*   Updated: 2023/01/04 11:44:25 by jebouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static void	print_sanitized(t_list *head)
+{
+	t_list	*temp;
+
+	temp = head;
+	while (temp)
+	{
+		while (temp && !ft_strncmp(temp->data, "x", 1))
+			temp = temp->next;
+		if (!temp)
+			break ;
+		ft_printf("%s\n", temp->data);
+		temp = temp->next;
+	}
+}
 
 int	main(int argc, char **argv)
 {
@@ -21,11 +37,12 @@ int	main(int argc, char **argv)
 	if (argc > 1)
 		read_inputs(&head, argc, argv);
 	if (head == NULL)
-		ft_putendl_fd("Error", 2);
+		error();
 	else
 	{
 		moves = sort_list(&head);
 		// print_str_list(moves);
+		// ft_printf("sanitized\n");
 		sanitize_moves(&moves);
 		print_sanitized(moves);
 		ft_lstclear(&head, &del_int_content);
